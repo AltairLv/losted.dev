bc2bbde60ef0964d4808a1a1b5cda0909f20c6e0
altairlv <notstaylost@gmail.com>
2023-12-25 13:46:58 +0100

    Update Spotify (yes again)

--- src/components/Card/Spotify/SpotifyServerCard.tsx
+++ src/components/Card/Spotify/SpotifyServerCard.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import clsx from "clsx";
-import { MusicIcon } from "@/components/Icons";
 import { ISpotifyPlay } from "@/types/types";
+import Link from "next/link";
 
 const SpotifyServerCard = ({ spotify }: { spotify: ISpotifyPlay }) => {
   return (
@@ -23,12 +23,12 @@ const SpotifyServerCard = ({ spotify }: { spotify: ISpotifyPlay }) => {
           className="rounded-sm object-contain hover:opacity-80"
         />
       </a>
-      <div className="flex flex-col w-fit max-w-[120px]">
+      <div className="flex flex-col w-fit max-w-[150px]">
         <a
           href={spotify.songUrl}
           rel="noopener noreferrer"
           target="_blank"
-          className="w-fit max-w-[120px] font-semibold text-sm truncate hover:underline underline-offset-2 hover:text-neutral-700 dark:hover:text-neutral-200"
+          className="w-fit max-w-[150px] font-semibold text-sm truncate hover:underline underline-offset-2 hover:text-neutral-700 dark:hover:text-neutral-200"
           title={spotify.title}
         >
           {spotify.title}
@@ -37,17 +37,22 @@ const SpotifyServerCard = ({ spotify }: { spotify: ISpotifyPlay }) => {
 
         <p className="text-xs truncate">by {spotify.artist}</p>
       </div>
-      <div>
-        <div
-          className={clsx(
-            spotify.isPlaying
-              ? "text-green-500 animate-swing"
-              : "text-orange-300"
-          )}
-        >
-          <MusicIcon />
-        </div>
-      </div>
+      <Link href={"/spotify"} prefetch={false} className="relative">
+        <span className="absolute -top-9 left-0.5 flex h-2.5 w-2.5">
+          <span
+            className={clsx(
+              spotify.isPlaying ? "animate-ping bg-green-500" : "bg-orange-300",
+              "absolute inline-flex h-full w-full rounded-full opacity-75"
+            )}
+          ></span>
+          <span
+            className={clsx(
+              spotify.isPlaying ? "bg-green-600" : "bg-orange-400",
+              "relative inline-flex rounded-full h-2.5 w-2.5 "
+            )}
+          ></span>
+        </span>
+      </Link>
     </div>
   );
 };
